c616d81e880993ee9747d727fda3d4677a9bd7b6
ZiyeAn <ziyean076@gmail.com>
2024-04-18 23:23:58 -0400

    file

---
+++ result2.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded',fetchDrinkData);
+const categoryChoice=localStorage.getItem("Category")
+console.log(categoryChoice);
+
+const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryChoice}`;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+async function fetchDrinkData() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const id = data.drinks[getRandomInt(20)].idDrink
+        console.log(id);
+        fetchDetailData(id)
+        //displayData(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+async function fetchDetailData(id) {
+    try {
+        const detaillUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+        const response = await fetch(detaillUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data); // Or process the data in any way you need
+        displayData(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+
+}
+function displayData(drinkData) {
+  const cocktailDiv = document.getElementById('drinks');
+  const name = drinkData.drinks[0].strDrink;
+  const ingredient1= drinkData.drinks[0].strIngredient1;
+  const ingredient2= drinkData.drinks[0].strIngredient2;
+  const ingredient3= drinkData.drinks[0].strIngredient3;
+  const ingredient4= drinkData.drinks[0].strIngredient4;
+  const instruciton= drinkData.drinks[0].strInstructions; 
+  cocktailDiv.innerHTML = `<p> name:${name} <br> ingredients:${ingredient1}, ${ingredient2}, ${ingredient3}, ${ingredient4} <br> instruciton:${instruciton}</p>`;
+}
+
---
+++ script.js
@@ -0,0 +1,55 @@
+
+//typewriter
+  var i = 0;
+  var j = 0;
+  var txt1 = 'I know what you want. Listen to your inner voice when choosing a divination tool. ';
+  var speed = 80;
+
+  function typeWriter1() {
+    if (i < txt1.length) {
+      document.getElementById("demo").innerHTML += txt1.charAt(i);
+      i++;
+      setTimeout(typeWriter1, speed);
+    }
+  }
+
+  function typeWriterOnLoad1() {
+      setTimeout(typeWriter1, 3500);
+    }
+
+  //api
+  let checkboxQ1 = document.querySelectorAll('.checkboxQ1'); 
+    for (var i = checkboxQ1.length - 1; i >= 0; i--) {
+        checkboxQ1[i].addEventListener('change',function(){
+            localStorage.setItem("baseLiquor", this.value)
+            const baseLiquorChoice=localStorage.getItem("baseLiquor")
+            console.log(baseLiquorChoice);
+        })
+    }
+
+//random coins
+    function pickRandom() {
+      var coins = [
+          { value: "Shake", image: "assets/IMG_4054.GIF", text:"three paws patterns" },
+          { value: "Cocktail", image: "assets/IMG_4053.GIF", text:"two paws and one liquor patterns" },
+          { value: "Coffee_/_Tea", image: "assets/IMG_4051.GIF", text:"two liquor and one paws patterns"},
+          { value: "Cocoa", image: "assets/IMG_4050.GIF", text:"three liquor patterns" }
+      ];
+
+      var randomCoins = coins[Math.floor(Math.random() * coins.length)];
+      var randomCoinsDiv = document.getElementById("randomCoins");
+      var textDiv = document.getElementById("CoinsDiv");
+      randomCoinsDiv.innerHTML = `
+          <img src="${randomCoins.image}" alt="Stick" style="position: absolute; width:100vw; height:auto; top:0%; left:0%">
+      `;
+      randomCoinsDiv.innerHTML = `
+          <div class="Instruction">
+          <p>Umm, you get ${randomCoins.text}.  I see...</p>
+          </div>
+      `;
+      localStorage.setItem("Category", randomCoins.value);
+  }
+  
+
+    
+  
